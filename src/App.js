cff01dcff3a5ffbcf56b585f1bb125cc1793805a
haripriya9209 <s.haripriya9209@gmail.com>
2022-08-03 17:38:40 +0530

    added install button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import './Qrscan/index'
 import QrScan from './Qrscan/index';
 import QrReader from './Qrreader';
+import InstallPWA from './InstallApp';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <div style={{marginTop:"50px"}}>
-          Welcome to Qr Scanner  
-        </div> 
-        {/* <QrScan/> */}
-        <QrReader/>
+      <header className='header'>
+        <InstallPWA/>
       </header>
+      <div className="centered">
+        <h3>Welcome to Scanner App</h3>
+        <QrReader/>
+        {/* <QrScan/> */}
+      </div>
+      
     </div>
   );
 }
